dbaa7655117d84fb809318409670b287efef4bb3
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-09-26 12:18:05 -0400

    cleaned up navbar links for consistancy

--- mario-kart-8p/src/components/Navbar/Navbar.js
+++ mario-kart-8p/src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 
 export default function Navbar() {
     return (
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/cups">Cups</a></li>
-            <li><a href="/tracks">All Tracks</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/cups">Cups</Link></li>
+            <li><Link to="/tracks">All Tracks</Link></li>
          </ul>
         </nav>
     );
