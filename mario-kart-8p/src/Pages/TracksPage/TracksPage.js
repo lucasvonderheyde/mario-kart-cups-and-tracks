9678e98aff17ea75ba4fb5c0a9f6c5391cad444b
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-09-27 14:42:01 -0400

    searchbar added as well as drop down, not fully functional yet

--- mario-kart-8p/src/Pages/TracksPage/TracksPage.js
+++ mario-kart-8p/src/Pages/TracksPage/TracksPage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Tracks from "../../components/Tracks/Tracks";
 import { useLoaderData } from "react-router-dom";
@@ -5,11 +6,48 @@ import { useLoaderData } from "react-router-dom";
 export default function TracksPage(){
    
     const tracksData = useLoaderData()
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const [filterTracks, setFilterTracks] = useState(tracksData)
+
+    function handleChange(event) {
+        const filteredTracksData = tracksData.filter(track => {
+          return track.name.toLowerCase().includes(event.target.value.toLowerCase())
+        })
+        setFilterTracks(previousValue => filteredTracksData)
+    }    
+    
+    const handleSelectChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    const collectionOfAppearances = []
+
+    tracksData.forEach((track) => {
+        track.appearances.forEach((appearance) => {
+          if (!collectionOfAppearances.includes(appearance)) {
+            collectionOfAppearances.push(appearance);
+          }
+        });
+      });
+      
+      console.log(collectionOfAppearances);
 
     return (
         <>
             <Navbar />
-            {tracksData.map(track => {
+            <input
+                type="text"
+                placeholder="Search for track"
+                onChange={handleChange}
+            />
+            <select value={selectedOption} onChange={handleSelectChange}>
+                <option value="">Select a Game</option>
+                {collectionOfAppearances.map(appearance => {
+                    return <option value={appearance}>{appearance}</option>
+                })}
+            </select>
+            {filterTracks.map(track => {
                 return <Tracks key={track.id} {...track} />
             })}
         </>
